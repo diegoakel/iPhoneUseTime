15fbccfec9edd080999f99ae842d3cd3267074ab
Diego Akel <diegoakel1@gmail.com>
2020-09-11 21:52:18 -0400

    a few corrections, but not there yet

--- main.py
+++ main.py
@@ -27,7 +27,10 @@ for x,b in enumerate(words):
 
 
 # Agora o reconhecimento do horario de cada app
+
+# Isso é tudo abaixo da palavra PlaceHolder
 complete_roi = img[ (int(position[1])+27) : height, 0: widht]
+
 height, widht,_ = complete_roi.shape
 apps_in_screen = height//77 # Quantos apps cabem na ROI escolhida. Esse número é o tamanho de cada região de APP
 
@@ -41,9 +44,14 @@ for app in list(range(apps_in_screen)):
     
     ROI = complete_roi[y1:y2, x1:x2]  
 
+    height, width,_ = ROI.shape
 
 
-    boxes =  (pytesseract.image_to_data(complete_roi, config=cong))
+    boxes =  (pytesseract.image_to_data(ROI, config=cong))
+    # cv2.imshow("Result", ROI)
+    # cv2.waitKey(0)
+
+    # boxes =  (pytesseract.image_to_data(complete_roi, config=cong))
     words = boxes.splitlines()
 
 
@@ -54,16 +62,38 @@ for app in list(range(apps_in_screen)):
             a = b.split()
             print (a)
             if len(a) == 12:
-                interno.append(a[-1])
-                # x,y,w,h = int(a[6]),int(a[7]),int(a[8]),int(a[9])
-                # cv2.rectangle(img,(x,y), (w+x, h+y), (0,0,255), 3)
+                # Primeiro retangulo
+                x,y,w,h = int(a[6]),int(a[7]),int(a[8]),int(a[9])
+                # cv2.rectangle(ROI,(x,y), (w+x, h+y), (0,0,255), 3)
+                # cv2.imshow("Result", ROI)
+                # cv2.waitKey(0)
+
+                x1,x2,y1,y2 = (w+x-86), (w+x), (y-2), (h+10)
+                # Região definida pelo quadrado com o tamanaho do horário
+                # Small_ROI = ROI[y:height, x:width]  
+                Small_ROI = ROI[y1:y2, x1:x2]  
+
+                cv2.imshow("Result", Small_ROI)
+                cv2.waitKey(0)
+
+
+                boxes =  (pytesseract.image_to_data(Small_ROI, config=cong))
+                words = boxes.splitlines()
+
+                # Varre a região pequena
+                for x,b in enumerate(words):
+                    if x!=0:
+                        a = b.split()
+                        print (a)
+                        if len(a) == 12:
+                            interno.append(a[-1])
 
                 # x1,x2,y1,y2 = (w+x-56), (w+x), (8), (9)
                 # print (x,y,w,h)
                 # print (x,y, w+x, h+y)
 
-    cv2.imshow("Result", ROI)
-    cv2.waitKey(0)
+    # cv2.imshow("Result", ROI)
+    # cv2.waitKey(0)
 
     apps.append(interno)
     
